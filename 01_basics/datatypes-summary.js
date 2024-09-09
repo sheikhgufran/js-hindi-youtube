4d069d6f4fe96edd0bebe36a795c0b9d4ae863d4
Md Gufran Alam <140594417+sheikhgufran@users.noreply.github.com>
2024-09-09 20:24:56 +0000

    change in location of file

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -12,7 +12,7 @@ let userEmail;
 const id = Symbol("123")
 const anotherId = Symbol("123")
 
-console.log(id == anotherId)
+// console.log(id == anotherId)
 
 
 // const bigNumber = 3446542347949499n;
@@ -36,6 +36,31 @@ const myfunction = function(){
 }
 
 
-console.log(typeof anotherId);
+// console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack(primitive type), Heap(Non-primitive)
+
+let myYoutubename = "hiteshchoudharydotcom"
+
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "mdgufranalam73210@gmail.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "hitesh@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
